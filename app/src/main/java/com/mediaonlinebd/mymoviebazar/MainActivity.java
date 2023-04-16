da656d8b870e4478413542cb51942de1075a273b
jibon <atikurrahaman386@gmail.com>
2023-04-17 03:06:17 +0600

    added main activity

---
+++ app/src/main/java/com/mediaonlinebd/mymoviebazar/MainActivity.java
@@ -0,0 +1,17 @@
+package com.mediaonlinebd.mymoviebazar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView webView = findViewById(R.id.webView);
+        webView.loadUrl("https://m.mymoviebazar.net");
+    }
+}
